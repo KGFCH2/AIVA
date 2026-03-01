c11202ea74856c0ed2b3df425900c75026e28a11
KGFCH2 <babinbid05@gmail.com>
2026-03-01 14:57:53 +0530

    All Done

--- backend/test_json.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('D:/Vs Code/PROJECT/AIVA/backend/data/responses.json'));
-console.log('Greetings keys:', Object.keys(data.greetings).length);
-console.log('Smalltalk keys:', Object.keys(data.smalltalk).length);
