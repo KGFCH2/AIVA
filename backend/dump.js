ccfc1b8bfb6f87bd3e9b356eb03d7dacd2d829ce
KGFCH2 <babinbid05@gmail.com>
2026-03-01 23:46:05 +0530

    Remove dump js

--- backend/dump.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const r = JSON.parse(fs.readFileSync('./data/responses.json', 'utf8'));
-console.log("Greetings:", Object.keys(r.greetings || {}).slice(0, 30));
-console.log("Smalltalk:", Object.keys(r.smalltalk || {}).slice(0, 30));
